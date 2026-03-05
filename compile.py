2a12216afc92475d84a3ec868adfc258069762ad
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-03-05 17:09:38 +1300

    Drop Copied Django Compile things

--- compile.py
+++
@@ -1,7 +0,0 @@
-from utils import compile_directory
-import os
-
-if __name__ == "__main__":
-    exclude_dirs = ["django_compiler", "env"]
-    directory = os.getcwd()
-    compile_directory(directory, exclude_dirs=exclude_dirs)
